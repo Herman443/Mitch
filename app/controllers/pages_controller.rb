e004ef4f3dadd9bd47ea014aba833557544abf78
Herman <hermanrql@gmail.com>
2017-07-12 10:20:13 +0000

    add own page for genres, fix dropdown-menu, remove genres

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,38 @@
 class PagesController < ApplicationController
   def home
-    @songs= Song.all.reverse_order
-    @latest_songs = @songs.last(6)
+    @songs= Song.all
+    @latest_songs = @songs.last(6).reverse
+  end
+  
+  def rock
+    @rock_songs = Song.all.where(genre: "Rock")
+  end
+  
+  def pop
+    @pop_songs = Song.all.where(genre: "Pop")
+  end
+  
+  def alternative_rock
+    @alternativ_rock_songs = Song.all.where(genre: "Alternative rock")
+  end
+  
+  def techno
+    @techno_songs = Song.all.where(genre: "Techno")
+  end
+  
+  def jazz
+    @jazz_songs = Song.all.where(genre: "Jazz")
+  end
+  
+  def classical
+    @classical_songs = Song.all.where(genre: "Clasical")
+  end
+  
+  def rap
+    @rap_songs = Song.all.where(genre: "Rap")
+  end
+  
+  def hip_hop
+    @hip_hop_songs = Song.all.where(genre: "Hip Hop")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   resources :songs
   
   root "pages#home"
+  
+  get "/rock" => "pages#rock"
+  get "/pop" => "pages#pop"
+  get "/alternative_rock" => "pages#alternative_rock"
+  get "/techno" => "pages#techno"
+  get "/jazz" => "pages#jazz"
+  get "/classical" => "pages#classical"
+  get "/rap" => "pages#rap"
+  get "/hip_hop" => "pages#hip_hop"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
